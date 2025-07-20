51232c30582861eaebf144ee58538909b853f22d
WilfriedMrc923 <ngouffowilfried237@gmail.com>
2025-07-21 01:34:39 +0200

    Ajoute les champs phone et franchisee_id dans le modèle User et sa migration

--- drivncook/backend/app/Models/User.php
+++ drivncook/backend/app/Models/User.php
@@ -2,31 +2,29 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
+     * Les attributs assignables en masse.
      */
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'password',
+        'role',
+        'franchisee_id',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
+     * Les attributs cachés lors de la sérialisation.
      */
     protected $hidden = [
         'password',
@@ -34,9 +32,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * Les attributs castés automatiquement.
      */
     protected function casts(): array
     {
@@ -44,5 +40,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Relation : un user franchisé appartient à un franchisé.
+     */
+    public function franchisee()
+    {
+        return $this->belongsTo(Franchisee::class);
     }
 }
