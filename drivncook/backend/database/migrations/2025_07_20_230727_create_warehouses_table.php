8d7f8233e15e9669e1e6a5aabc9a7a0adca5522c
WilfriedMrc923 <ngouffowilfried237@gmail.com>
2025-07-21 01:08:14 +0200

    Ajoute l'entité Warehouse (entrepôt) avec infos complètes et capacités

---
+++ drivncook/backend/database/migrations/2025_07_20_230727_create_warehouses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+        public function up(): void
+    {
+        Schema::create('warehouses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('region');
+            $table->string('phone');
+            $table->string('manager_name');
+            $table->string('email')->unique();
+            $table->integer('capacity');
+            $table->boolean('kitchen_available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('warehouses');
+    }
+};
