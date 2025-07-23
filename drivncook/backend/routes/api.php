704607ea15375c6cef946c72f6de29edc357a4d4
WilfriedMrc923 <ngouffowilfried237@gmail.com>
2025-07-23 02:11:11 +0200

    Enregistrement de la route d'authentification API pour les clients

--- drivncook/backend/routes/api.php
+++ drivncook/backend/routes/api.php
@@ -6,8 +6,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CustomerAuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/customer/login', [CustomerAuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', function (Request $request) {
