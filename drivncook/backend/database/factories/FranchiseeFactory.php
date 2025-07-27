b859e8f93071a86dc74f126dac06c5d28de73577
WilfriedMrc923 <ngouffowilfried237@gmail.com>
2025-07-27 21:15:06 +0200

    AJOUT

--- drivncook/backend/database/factories/FranchiseeFactory.php
+++ drivncook/backend/database/factories/FranchiseeFactory.php
@@ -2,22 +2,28 @@
 
 namespace Database\Factories;
 
+use App\Models\Franchisee;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Franchisee>
- */
 class FranchiseeFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Franchisee::class;
+
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->company,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'city' => $this->faker->city,
+            'postal_code' => $this->faker->postcode,
+            'country' => 'France',
+            'siret_number' => $this->faker->numerify('##############'), // 14 chiffres
+            'franchise_code' => strtoupper(Str::random(8)), // Code aléatoire unique
+            'entry_fee_paid' => true,
+            'sales_percentage' => 4.00
         ];
     }
 }
