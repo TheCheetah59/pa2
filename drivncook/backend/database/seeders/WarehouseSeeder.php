5d221089b5ba9ea8f211bdb4dcadf0c905560366
WilfriedMrc923 <ngouffowilfried237@gmail.com>
2025-07-31 10:34:03 +0200

    modif

--- drivncook/backend/database/seeders/WarehouseSeeder.php
+++ drivncook/backend/database/seeders/WarehouseSeeder.php
@@ -15,7 +15,13 @@ class WarehouseSeeder extends Seeder
             Warehouse::create([
                 'name' => 'Entrepôt ' . $region,
                 'address' => fake()->address(),
+                'postal_code' => fake()->postcode(),
+                'city' => fake()->city(),
                 'region' => $region,
+                'phone' => fake()->phoneNumber(),
+                'email' => fake()->unique()->safeEmail(),
+                'capacity' => fake()->numberBetween(50, 200),
+                'kitchen_available' => fake()->boolean(),
             ]);
         }
     }
