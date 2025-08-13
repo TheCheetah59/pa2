85ae39189db6c00801b73b4ea3d0c1bd9d89ae76
WilfriedMrc923 <ngouffowilfried237@gmail.com>
2025-08-14 00:51:02 +0200

    modif

--- drivncook/frontend/src/Routes.jsx
+++ drivncook/frontend/src/Routes.jsx
@@ -3,13 +3,21 @@ import Accueil from "./pages/Accueil";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Menu from "./pages/Menu"; // ou Dashboard
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Accueil />} />
     <Route path="/login" element={<Login />} />
     <Route path="/register" element={<Register />} />
-    <Route path="/menu" element={<Menu />} />
+    <Route
+      path="/menu"
+      element={
+        <ProtectedRoute>
+          <Menu />
+        </ProtectedRoute>
+      }
+    />
   </Routes>
 );
 
