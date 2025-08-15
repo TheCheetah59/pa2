999f907299e85aa629bb84fd15239132bf88e044
WilfriedMrc923 <ngouffowilfried237@gmail.com>
2025-08-15 23:02:27 +0200

    MODIF

---
+++ drivncook/backend/app/Http/Controllers/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of users pending activation.
+     */
+    public function index()
+    {
+        return User::where('is_activated', false)->get();
+    }
+
+    /**
+     * Activate the given user account.
+     */
+    public function activate(User $user)
+    {
+        $user->is_activated = true;
+        $user->save();
+
+        return response()->json(['message' => 'User activated successfully', 'user' => $user]);
+    }
+
+    /**
+     * Suspend the given user account.
+     */
+    public function suspend(User $user)
+    {
+        $user->is_activated = false;
+        $user->save();
+
+        return response()->json(['message' => 'User suspended successfully', 'user' => $user]);
+    }
+}
--- drivncook/backend/app/Http/Controllers/UserController.php
+++ drivncook/backend/app/Http/Controllers/UserController.php
@@ -6,33 +6,5 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of users pending activation.
-     */
-    public function index()
-    {
-        return User::where('is_activated', false)->get();
-    }
-
-    /**
-     * Activate the given user account.
-     */
-    public function activate(User $user)
-    {
-        $user->is_activated = true;
-        $user->save();
-
-        return response()->json(['message' => 'User activated successfully', 'user' => $user]);
-    }
-
-    /**
-     * Suspend the given user account.
-     */
-    public function suspend(User $user)
-    {
-        $user->is_activated = false;
-        $user->save();
-
-        return response()->json(['message' => 'User suspended successfully', 'user' => $user]);
-    }
+    // User-specific actions can be implemented here.
 }
