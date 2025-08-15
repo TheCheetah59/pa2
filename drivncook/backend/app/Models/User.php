4a59e70e8153799682ac09caeee391d6f8ecb5f6
WilfriedMrc923 <ngouffowilfried237@gmail.com>
2025-08-15 20:47:45 +0200

    modif

--- drivncook/backend/app/Models/User.php
+++ drivncook/backend/app/Models/User.php
@@ -11,6 +11,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public const ROLE_ADMIN = 'admin';
+    public const ROLE_CLIENT = 'client';
+    public const ROLE_FRANCHISE = 'franchise';
+
     protected $fillable = [
         'name',
         'email',
@@ -18,15 +22,22 @@ class User extends Authenticatable
         'password',
         'role',
         'franchisee_id',
+        'is_activated',
+        'activation_token',
+        'activation_token_expires_at',
     ];
 
     protected $hidden = [
         'password',
         'remember_token',
+        'activation_token',
+        'activation_token_expires_at',
     ];
 
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'is_activated' => 'boolean',
+        'activation_token_expires_at' => 'datetime',
     ];
 
     public function franchisee()
